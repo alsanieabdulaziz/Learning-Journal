c2cd619c87cc3e31b59d1fa1bee030cf0f4be433
Abdulaziz Alsanie <42047525+alsanieabdulaziz@users.noreply.github.com>
2019-02-20 17:07:34 +0300

    Update PersonalMessage2-3PCC.py

--- Python Crash Course-Exercises/PersonalMessage2-3PCC.py
+++ Python Crash Course-Exercises/PersonalMessage2-3PCC.py
@@ -1,7 +1,3 @@
 name=input("What is your name? ")
-try:
-    name=str(name)
-    name=name.strip()
-except:
-    print("Please input a name")
+name=name.strip()
 print("Hello", name.title() +  ', would you like to learn Python today?')
